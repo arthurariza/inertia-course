a679307def9c42f9fd597d9d68ad9b85712b3c61
arthurariza <tucaariza@gmail.com>
2023-03-09 12:03:26 -0300

    Pages https://inertiajs.com/pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Welcome, {{ name }}</h1>
+
+    <p>Frameworks we will use:</p>
+    <ul>
+        <li v-for="framework in frameworks" v-text="framework"></li>
+    </ul>
+</template>
+
+<script setup>
+defineProps({
+    'name': String,
+    'frameworks': Array
+});
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Inertia('Welcome');
+    return Inertia::render('Home', [
+        'name' => 'Arthur',
+        'frameworks' => ['Laravel', 'Vue', 'Inertia']
+    ]);
 });
